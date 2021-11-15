ad5bc2055ab06f915c0c3ba8b97b9d48308edf88
Oliver Trabhardt <oliver.trabhardt@googlemail.com>
2021-11-15 13:42:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Checklist from "./components/Checklist";
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import './index.css';
 
 ReactDOM.render(<Checklist />, document.getElementById("root"));
